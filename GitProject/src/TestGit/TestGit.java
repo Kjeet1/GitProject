84d5734a7da97586cfd12678ff97cff3df8b36e7
Kjeet1 <74446319+Kjeet1@users.noreply.github.com>
2021-03-25 17:26:23 -0700

    I added a new statement

--- GitProject/src/TestGit/TestGit.java
+++ GitProject/src/TestGit/TestGit.java
@@ -4,7 +4,8 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
-		System.out.println("How are you?")
+		System.out.println("How are you?");
+		System.out.print("I am fine");
 
 	}
 
